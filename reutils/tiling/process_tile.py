53ed27fc5df422b139600f14caf7e5af03457b4b
umzi <131161226+umzi2@users.noreply.github.com>
2025-04-24 14:51:44 +0300

    Update process_tile.py

--- reutils/tiling/process_tile.py
+++ reutils/tiling/process_tile.py
@@ -66,44 +66,39 @@ def process_tiles(
     tile_size = tiler.starting_tile_size(w, h, c)
 
     model = model.to(device, dtype=dtype).eval()
+    with torch.inference_mode():
+        result_blender = TileBlender(h * scale, w * scale, c, BlendDirection.Y)
+        y_segments = split_into_segments(h, tile_size[1], overlap)
 
-    while True:
-        try:
-            result_blender = TileBlender(h * scale, w * scale, c, BlendDirection.Y)
-            y_segments = split_into_segments(h, tile_size[1], overlap)
+        for y_seg in y_segments:
+            y_start = y_seg.start - y_seg.start_padding
+            y_end = y_seg.end + y_seg.end_padding
+            row = img[y_start:y_end, :, :]
 
-            for y_seg in y_segments:
-                y_start = y_seg.start - y_seg.start_padding
-                y_end = y_seg.end + y_seg.end_padding
-                row = img[y_start:y_end, :, :]
+            row_blender = TileBlender((y_end - y_start) * scale, w * scale, c, BlendDirection.X)
 
-                row_blender = TileBlender((y_end - y_start) * scale, w * scale, c, BlendDirection.X)
+            x_segments = split_into_segments(w, tile_size[0], overlap)
+            for x_seg in x_segments:
+                x_start = x_seg.start - x_seg.start_padding
+                x_end = x_seg.end + x_seg.end_padding
+                tile = row[:, x_start:x_end, :]
+                tensor = image2tensor(tile, dtype=dtype).to(device)
+                with torch.autocast(device_type=str(device), dtype=dtype, enabled=amp):
+                    
+                        tensor = model(tensor)
 
-                x_segments = split_into_segments(w, tile_size[0], overlap)
-                for x_seg in x_segments:
-                    x_start = x_seg.start - x_seg.start_padding
-                    x_end = x_seg.end + x_seg.end_padding
-                    tile = row[:, x_start:x_end, :]
-                    tensor = image2tensor(tile, dtype=dtype).to(device)
-                    with torch.autocast(device_type=str(device), dtype=dtype, enabled=amp):
-                        with torch.inference_mode():
-                            tensor = model(tensor)
+                processed_tile = tensor2image(tensor)
 
-                    processed_tile = tensor2image(tensor)
+                x_overlap = TileOverlap(start=x_seg.start_padding * scale, end=x_seg.end_padding * scale)
+                row_blender.add_tile(processed_tile, x_overlap)
 
-                    x_overlap = TileOverlap(start=x_seg.start_padding * scale, end=x_seg.end_padding * scale)
-                    row_blender.add_tile(processed_tile, x_overlap)
+            processed_row = row_blender.get_result()
 
-                processed_row = row_blender.get_result()
+            y_overlap = TileOverlap(start=y_seg.start_padding * scale, end=y_seg.end_padding * scale)
+            result_blender.add_tile(processed_row, y_overlap)
+            del processed_row, y_overlap
 
-                y_overlap = TileOverlap(start=y_seg.start_padding * scale, end=y_seg.end_padding * scale)
-                result_blender.add_tile(processed_row, y_overlap)
-                del processed_row, y_overlap
+        result = result_blender.get_result()
+ 
 
-            result = result_blender.get_result()
-            break
-            
-        except torch.cuda.OutOfMemoryError:
-            tile_size = tiler.split(tile_size)
-            torch.cuda.empty_cache()
     return result.squeeze()
