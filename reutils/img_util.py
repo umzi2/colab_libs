5b0d0fe7340b9351f7b6b4355d8526a704ec9cec
umzi <131161226+umzi2@users.noreply.github.com>
2025-04-24 14:35:54 +0300

    Update img_util.py

--- reutils/img_util.py
+++ reutils/img_util.py
@@ -10,44 +10,33 @@ def get_h_w_c(img: np.ndarray):
 
 
 def image2tensor(
-    value: list[np.ndarray] | np.ndarray,
+    img: np.ndarray,
     dtype: torch.dtype = torch.float32,
 ) -> list[Tensor] | Tensor:
-    def _to_tensor(img: np.ndarray) -> torch.Tensor:
-        if len(img.shape) == 2:
-            img = torch.from_numpy(img[None, ...])
-        else:
-            img = torch.from_numpy(img).permute(2, 0, 1)
+    if len(img.shape) == 2:
+        img = torch.from_numpy(img[None, ...])
+    else:
+        img = torch.from_numpy(img).permute(2, 0, 1)
 
-        img.unsqueeze_(0)
+    unf = img.unsqueeze(0)
 
-        if img.dtype != dtype:
-            img = img.to(dtype)
-
-        return img
-
-    if isinstance(value, list):
-        return [_to_tensor(i) for i in value]
-    return _to_tensor(value)
+    if img.dtype != dtype:
+        img = img.to(dtype)
+    return img
 
 
 def tensor2image(
-    value: list[torch.Tensor] | torch.Tensor,
+    tensor: list[torch.Tensor] | torch.Tensor,
     dtype=np.float32,
 ) -> list[np.ndarray] | np.ndarray:
-    def _to_ndarray(tensor: torch.Tensor) -> np.ndarray:
-        tensor = tensor.squeeze(0).detach().cpu()
+    tensor = tensor.squeeze(0).detach().cpu()
 
-        if tensor.dtype != torch.float32:
-            tensor = tensor.float()
+    if tensor.dtype != torch.float32:
+        tensor = tensor.float()
 
-        if len(tensor.shape) == 2:
-            tensor = tensor.numpy()
-        else:
-            tensor = tensor.permute(1, 2, 0).numpy()
+    if len(tensor.shape) == 2:
+        tensor = tensor.numpy()
+    else:
+        tensor = tensor.permute(1, 2, 0).numpy()
 
-        return tensor.astype(dtype)
-
-    if isinstance(value, list):
-        return [_to_ndarray(i) for i in value]
-    return _to_ndarray(value)
+    return tensor.astype(dtype)
